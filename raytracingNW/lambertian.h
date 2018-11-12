cd8ba1b2687821a63c29aed469d38c70cb15da20
栾盼 <luanpan@aobi.com>
2018-11-12 09:05:06 +0800

    3 solid texture 4 perlin noise

--- raytracingNW/lambertian.h
+++ raytracingNW/lambertian.h
@@ -2,27 +2,20 @@
 #define LAMBERTIANH
 
 #include "material.h"
+#include "texture.h"
 
 class lambertian :public material {
 public:
-	lambertian(const vec3& a) :albedo(a) {}
+	lambertian(texture* a) :albedo(a) {}
 	virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered)const {
 		vec3 target = rec.p + rec.normal + random_in_unit_sphere();
 		scattered = ray(rec.p, target - rec.p);
-		attenuation = albedo;
+		attenuation = albedo->value(0,0,rec.p);
 		return true;
 	}
-	vec3 albedo;
+	texture *albedo;
 };
 
-
-
-//bool lambertian::scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered)const {
-//	vec3 target = rec.p + rec.normal + random_in_unit_sphere();
-//	scattered = ray(rec.p, target - rec.p);
-//	attenuation = albedo;
-//	return true;
-//}
 
 #endif // !LAMBERTIANH
 
--- raytracingNW/main.cpp
+++ raytracingNW/main.cpp
@@ -16,6 +16,8 @@
 #include "omp.h"
 #include <vector>
 #include "lodepng.h"
+#include "texture.h"
+#include <string>
 
 using namespace std;
 time_t start, stop;
@@ -23,8 +25,8 @@ time_t start, stop;
 hitable *random_scene() {
 	int n = 5000;
 	hitable **list = new hitable *[n + 1];
-	/*定义一个包含n+1个元素的数组，数组的每个元素是指向hitable对象的指针。然后将数组的指针赋值给list。所以，list是指针的指针。*/
-	list[0] = new sphere(vec3(0, -1000, 0), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
+	texture *checker = new checker_texture(new constant_texture(vec3(0.2, 0.3, 0.1)), new constant_texture(vec3(0.9, 0.9, 0.9)));
+	list[0] = new sphere(vec3(0, -1000, 0), 1000, new lambertian(checker));
 	/*先创建一个中心在（0，-1000，0）半径为1000的超大漫射球，将其指针保存在list的第一个元素中。*/
 	int i = 1;
 	for (int a = -10; a < 10; a++) {
@@ -39,7 +41,7 @@ hitable *random_scene() {
 				if (choose_mat < 0.8) {     //diffuse  
 					/*材料阀值小于0.8，则设置为漫反射球，漫反射球的衰减系数x,y,z都是（0，1）之间的随机数的平方*/
 					list[i++] = new moving_sphere(center, center+vec3(0, 0.5*drand48(), 0), 0.0, 1.0, 0.2,
-						new lambertian(vec3(drand48()*drand48(), drand48()*drand48(), drand48()*drand48())));
+						new lambertian(new constant_texture(vec3(drand48()*drand48(), drand48()*drand48(), drand48()*drand48()))));
 				}
 				else if (choose_mat < 0.95) {
 					/*材料阀值大于等于0.8小于0.95，则设置为镜面反射球，镜面反射球的衰减系数x,y,z及模糊系数都是（0，1）之间的随机数加一再除以2*/
@@ -58,23 +60,30 @@ hitable *random_scene() {
 	}
 
 	list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
-	list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
+	list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(new constant_texture(vec3(0.4, 0.2, 0.1))));
 	list[i++] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
 	/*定义三个大球*/
 	return new bvh_node(list, i, 0.0, 1.0);
 }
 
-hitable *random_scene2() {
-	int n = 5;
-	hitable **list = new hitable *[n + 1];
-	int i = 1;
-	
-	list[0] = new sphere(vec3(0, -1000, 0), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
-	list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
-	list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
-	list[i++] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
-	printf("random_scene2 i = %d\n", i);
-	return new bvh_node(list, i, 0.0, 1.0);
+hitable *two_sphere(char** name) {
+	texture *checker = new checker_texture(new constant_texture(vec3(0.2, 0.3, 0.1)), new constant_texture(vec3(0.9, 0.9, 0.9)));
+	int n = 50;
+	hitable **list = new hitable*[n + 1];
+	list[0] = new sphere(vec3(0, -10, 0), 10, new lambertian(checker));
+	list[1] = new sphere(vec3(0, 10, 0), 10, new lambertian(checker));
+	*name = "img//two_sphere.png";
+	return new hitable_list(list, 2);
+}
+
+hitable *two_perlin_sphere(char** name) {
+	texture *pertext = new noise_texture(5);
+
+	hitable **list = new hitable*[2];
+	list[0] = new sphere(vec3(0, -1000, 0), 1000, new lambertian(pertext));
+	list[1] = new sphere(vec3(0, 2, 0), 2, new lambertian(pertext));
+	*name = "img//two_perlin_sphere_turb.png";
+	return new hitable_list(list, 2);
 }
 
 vec3 color(const ray& r, hitable *world, int depth) {
@@ -98,27 +107,24 @@ vec3 color(const ray& r, hitable *world, int depth) {
 
 void encodeOneStep(const char* filename, std::vector<unsigned char>& image, unsigned width, unsigned height)
 {
-	//Encode the image
 	unsigned error = lodepng::encode(filename, image, width, height);
-
-	//if there's an error, display it
 	if (error) std::cout << "encoder error " << error << ": " << lodepng_error_text(error) << std::endl;
 }
 
 int main() {
 	start = time(NULL);
-	const int nx = 200;
-	const int ny = 100;
+	const int nx = 1000;
+	const int ny = 500;
 	int ns = 100;
 	std::vector<unsigned char> image;
 	image.resize(nx * ny * 4);
 	//ofstream ppmfile("img\\12\\2.ppm");
 	//ppmfile << "P3\n" << nx << " " << ny << "\n255\n";
-
-	hitable *world = random_scene();
+	char* fileName = "img//no_name.png";
+	hitable *world = two_perlin_sphere(&fileName);
 	vec3 lookfrom(13, 2, 3);
 	vec3 lookat(0, 0, 0);
-	float dist_to_focus = (lookfrom - lookat).length();
+	float dist_to_focus = 10;
 	float aperture = 0;
 	camera cam(lookfrom, lookat, vec3(0, 1, 0), 20, float(nx) / float(ny), aperture, dist_to_focus,0.0, 1.0);
 
@@ -139,10 +145,7 @@ int main() {
 			}
 			col /= float(ns);
 			col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
-			//int ir = int(255.99*col[0]);
-			//int ig = int(255.99*col[1]);
-			//int ib = int(255.99*col[2]);
-			//ppmfile << ir << " " << ig << " " << ib << "\n" << endl;
+
 			image[4 * nx * (ny - j-1) + 4 * i + 0] = int(255.99*col[0]);
 			image[4 * nx * (ny - j-1) + 4 * i + 1] = int(255.99*col[1]);
 			image[4 * nx * (ny - j-1) + 4 * i + 2] = int(255.99*col[2]);
@@ -150,7 +153,7 @@ int main() {
 		}
 	}
 
-	encodeOneStep("img//01.png", image, nx, ny);
+	encodeOneStep(fileName, image, nx, ny);
 		
 	//ppmfile.close();
 	stop = time(NULL);
